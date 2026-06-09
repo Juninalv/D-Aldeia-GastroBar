7bf355573e861d8275455924b6ad2800d5e9163d
Juninho Alves <junioralv290302@gmail.com>
2026-06-09 10:57:06 -0300

    adiciona zoom na imagem da galeria

--- javascript/main.js
+++ javascript/main.js
@@ -48,3 +48,79 @@ if (prev && slides.length) {
     showSlide(current);
   });
 }
+
+const imagensGaleria = document.querySelectorAll(".slide-track img");
+
+const lightbox = document.querySelector(".lightbox");
+const lightboxImg = document.querySelector(".lightbox-img");
+const fecharLightbox = document.querySelector(".lightbox-fechar");
+
+const btnPrev = document.querySelector(".lightbox-prev");
+const btnNext = document.querySelector(".lightbox-next");
+
+let imagemAtual = 0;
+
+// ABRIR IMAGEM
+
+imagensGaleria.forEach((imagem, index) => {
+  imagem.addEventListener("click", () => {
+    imagemAtual = index;
+
+    lightbox.classList.add("ativo");
+    lightboxImg.src = imagem.src;
+  });
+});
+
+// FECHAR NO X
+
+fecharLightbox.addEventListener("click", () => {
+  lightbox.classList.remove("ativo");
+});
+
+// FECHAR CLICANDO FORA
+
+lightbox.addEventListener("click", (e) => {
+  if (e.target === lightbox) {
+    lightbox.classList.remove("ativo");
+  }
+});
+
+// PRÓXIMA FOTO
+
+btnNext.addEventListener("click", () => {
+  imagemAtual++;
+
+  if (imagemAtual >= imagensGaleria.length) {
+    imagemAtual = 0;
+  }
+
+  lightboxImg.src = imagensGaleria[imagemAtual].src;
+});
+
+// FOTO ANTERIOR
+
+btnPrev.addEventListener("click", () => {
+  imagemAtual--;
+
+  if (imagemAtual < 0) {
+    imagemAtual = imagensGaleria.length - 1;
+  }
+
+  lightboxImg.src = imagensGaleria[imagemAtual].src;
+});
+
+document.addEventListener("keydown", (e) => {
+  if (!lightbox.classList.contains("ativo")) return;
+
+  if (e.key === "ArrowRight") {
+    btnNext.click();
+  }
+
+  if (e.key === "ArrowLeft") {
+    btnPrev.click();
+  }
+
+  if (e.key === "Escape") {
+    lightbox.classList.remove("ativo");
+  }
+});
